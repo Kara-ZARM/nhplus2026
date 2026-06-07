c65e0ce5f9dd426b8beff751cbab0e6819ca7e16
Ari <ali.s22@schule.bremen.de>
2026-06-07 12:27:45 +0200

    erstellt: AllCaregiverController, AllCaregiverView. angepasst: MainWindowView und MainWindowController

--- src/main/java/de/hitec/nhplus/controller/AllCaregiverController.java
+++ src/main/java/de/hitec/nhplus/controller/AllCaregiverController.java
@@ -1,4 +1,317 @@
 package de.hitec.nhplus.controller;
 
+import de.hitec.nhplus.datastorage.DaoFactory;
+import de.hitec.nhplus.datastorage.CaregiverDao;
+import de.hitec.nhplus.model.Caregiver;
+import de.hitec.nhplus.utils.DateConverter;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+/**
+ * The <code>AllCaregiverController</code> contains the entire logic of the caregiver view. It determines which data is displayed and how to react to events.
+ */
 public class AllCaregiverController {
+    @FXML
+    private TableView<Caregiver> tableView;
+
+    @FXML
+    private TableColumn<Caregiver, Integer> columnId;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnFirstName;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnSurname;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnPhone;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnDateOfBirth;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnStreet;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnPostalCode;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnCity;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnTaxClass;
+
+    @FXML
+    private TableColumn<Caregiver, String> columnQualification;
+
+    @FXML
+    private Button buttonDelete;
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private TextField textFieldSurname;
+
+    @FXML
+    private TextField textFieldFirstName;
+
+    @FXML
+    private TextField textFieldPhone;
+
+    @FXML
+    private TextField textFieldDateOfBirth;
+
+    @FXML
+    private TextField textFieldStreet;
+
+    @FXML
+    private TextField textFieldPostalCode;
+
+    @FXML
+    private TextField textFieldCity;
+
+    @FXML
+    private TextField textFieldTaxClass;
+
+    @FXML
+    private TextField textFieldQualification;
+
+    private final ObservableList<Caregiver> caregiver = FXCollections.observableArrayList();
+    private CaregiverDao dao;
+
+    /**
+     * When <code>initialize()</code> gets called, all fields are already initialized. For example from the FXMLLoader
+     * after loading an FXML-File. At this point of the lifecycle of the Controller, the fields can be accessed and
+     * configured.
+     */
+
+    public void initialize() {
+        this.readAllAndShowInTableView();
+
+        this.columnId.setCellValueFactory(new PropertyValueFactory<>("cid"));
+
+        // CellValueFactory to show property values in TableView
+        this.columnFirstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        // CellFactory to write property values from with in the TableView
+        this.columnFirstName.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnSurname.setCellValueFactory(new PropertyValueFactory<>("surname"));
+        this.columnSurname.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        this.columnPhone.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnDateOfBirth.setCellValueFactory(new PropertyValueFactory<>("dateOfBirth"));
+        this.columnDateOfBirth.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnStreet.setCellValueFactory(new PropertyValueFactory<>("street"));
+        this.columnStreet.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnPostalCode.setCellValueFactory(new PropertyValueFactory<>("postalcode"));
+        this.columnPostalCode.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnCity.setCellValueFactory(new PropertyValueFactory<>("city"));
+        this.columnCity.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnTaxClass.setCellValueFactory(new PropertyValueFactory<>("taxclass"));
+        this.columnTaxClass.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnQualification.setCellValueFactory(new PropertyValueFactory<>("qualification"));
+        this.columnQualification.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        //Anzeigen der Daten
+        this.tableView.setItems(this.caregiver);
+
+        this.buttonDelete.setDisable(true);
+        this.tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Caregiver>() {
+            @Override
+            public void changed(ObservableValue<? extends Caregiver> observableValue, Caregiver oldCaregiver, Caregiver newCaregiver) {
+
+                AllCaregiverController.this.buttonDelete.setDisable(newCaregiver == null);
+            }
+        });
+
+        this.buttonAdd.setDisable(true);
+        ChangeListener<String> inputNewCaregiverListener = (observableValue, oldText, newText) ->
+                AllCaregiverController.this.buttonAdd.setDisable(!AllCaregiverController.this.areInputDataValid());
+        this.textFieldSurname.textProperty().addListener(inputNewCaregiverListener);
+        this.textFieldFirstName.textProperty().addListener(inputNewCaregiverListener);
+        this.textFieldPhone.textProperty().addListener(inputNewCaregiverListener);
+        this.textFieldDateOfBirth.textProperty().addListener(inputNewCaregiverListener);
+        this.textFieldStreet.textProperty().addListener(inputNewCaregiverListener);
+        this.textFieldPostalCode.textProperty().addListener(inputNewCaregiverListener);
+        this.textFieldCity.textProperty().addListener(inputNewCaregiverListener);
+        this.textFieldTaxClass.textProperty().addListener(inputNewCaregiverListener);
+        this.textFieldQualification.textProperty().addListener(inputNewCaregiverListener);
+    }
+
+    /**
+     * When a cell of the column with first names was changed, this method will be called, to persist the change.
+     *
+     * @param event Event including the changed object and the change.
+     */
+
+    @FXML
+    public void handleOnEditFirstname(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setFirstName(event.getNewValue());
+        this.doUpdate(event);
+    }
+
+    /**
+     * When a cell of the column with surnames was changed, this method will be called, to persist the change.
+     *
+     * @param event Event including the changed object and the change.
+     */
+    @FXML
+    public void handleOnEditSurname(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setSurname(event.getNewValue());
+        this.doUpdate(event);
+    }
+
+    /**
+     * When a cell of the column with dates of birth was changed, this method will be called, to persist the change.
+     *
+     * @param event Event including the changed object and the change.
+     */
+    @FXML
+    public void handleOnEditDateOfBirth(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setDateOfBirth(event.getNewValue());
+        this.doUpdate(event);
+    }
+
+    /**
+     * When a cell of the column with taxclass was changed, this method will be called, to persist the change.
+     *
+     * @param event Event including the changed object and the change.
+     */
+    @FXML
+    public void handleOnEditAssets(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setTaxclass(event.getNewValue());
+        this.doUpdate(event);
+    }
+
+    /**
+     * When a cell of the column with qualification was changed, this method will be called, to persist the change.
+     *
+     * @param event Event including the changed object and the change.
+     */
+    @FXML
+    public void handleOnEditAssets(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setQualification(event.getNewValue());
+        this.doUpdate(event);
+    }
+
+    /**
+     * Updates a caregiver by calling the method <code>update()</code> of {@link CaregiverDao}.
+     *
+     * @param event Event including the changed object and the change.
+     */
+    private void doUpdate(TableColumn.CellEditEvent<Caregiver, String> event) {
+        try {
+            this.dao.update(event.getRowValue());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Reloads all caregiver to the table by clearing the list of all caregiver and filling it again by all persisted
+     * caregiver, delivered by {@link CaregiverDao}.
+     */
+    private void readAllAndShowInTableView() {
+        this.caregiver.clear();
+        this.dao = DaoFactory.getDaoFactory().createCaregiverDao();
+        try {
+            this.caregiver.addAll(this.dao.readAll());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * This method handles events fired by the button to delete caregiver. It calls {@link CaregiverDao} to delete the
+     * caregiver from the database and removes the object from the list, which is the data source of the
+     * <code>TableView</code>.
+     */
+    @FXML
+    public void handleDelete() {
+        Caregiver selectedItem = this.tableView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            try {
+                DaoFactory.getDaoFactory().createCaregiverDao().deleteById(selectedItem.getPid());
+                this.tableView.getItems().remove(selectedItem);
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * This method handles the events fired by the button to add a caregiver. It collects the data from the
+     * <code>TextField</code>s, creates an object of class <code>Caregiver</code> of it and passes the object to
+     * {@link CaregiverDao} to persist the data.
+     */
+    @FXML
+    public void handleAdd() {
+        String surname = this.textFieldSurname.getText();
+        String firstName = this.textFieldFirstName.getText();
+        String birthday = this.textFieldDateOfBirth.getText();
+        LocalDate date = DateConverter.convertStringToLocalDate(birthday);
+        String street = this.textFieldStreet.getText();
+        int postalCode = Integer.parseInt(this.textFieldPostalCode.getText());
+        String city = this.textFieldCity.getText();
+        String roomNumber = this.textFieldTaxClass.getText();
+        String assets = this.textFieldQualification.getText();
+        try {
+            this.dao.create(new Caregiver(firstName, surname, date, careLevel, roomNumber, assets));
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        readAllAndShowInTableView();
+        clearTextfields();
+    }
+
+    /**
+     * Clears all contents from all <code>TextField</code>s.
+     */
+    private void clearTextfields() {
+        this.textFieldFirstName.clear();
+        this.textFieldSurname.clear();
+        this.textFieldDateOfBirth.clear();
+        this.textFieldStreet.clear();
+        this.textFieldPostalCode.clear();
+        this.textFieldCity.clear();
+        this.textFieldTaxClass.clear();
+        this.textFieldQualification.clear();
+    }
+
+    private boolean areInputDataValid() {
+        if (!this.textFieldDateOfBirth.getText().isBlank()) {
+            try {
+                DateConverter.convertStringToLocalDate(this.textFieldDateOfBirth.getText());
+            } catch (Exception exception) {
+                return false;
+            }
+        }
+
+        return !this.textFieldFirstName.getText().isBlank() && !this.textFieldSurname.getText().isBlank() &&
+                !this.textFieldDateOfBirth.getText().isBlank() && !this.textFieldStreet.getText().isBlank() &&
+                !this.textFieldPostalCode.getText().isBlank() && !this.textFieldCity.getText().isBlank() &&
+                !this.textFieldTaxClass.getText().isBlank() && !this.textFieldQualification.getText().isBlank();
+    }
+
+
 }
