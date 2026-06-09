a8ac0aa166ec243645ea235f6995a108b7dd10ed
Ari <ali.s22@schule.bremen.de>
2026-06-09 12:19:28 +0200

    fix: refactor alert methods to own class(alertBuilder) and add java docs

---
+++ src/main/java/de/hitec/nhplus/utils/AlertBuilder.java
@@ -0,0 +1,36 @@
+package de.hitec.nhplus.utils;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+/**
+ * This Class holds methods to build alerts when trying to delete or edit a patients or caregivers data
+ */
+
+public class AlertBuilder {
+
+    /**
+     * This method handles events fired when editing fields for patients or caregivers. It opens an alert when user tries to save an empty field.
+     * On confirmation (press 'OK')  {@link } is called.
+     */
+
+    public static Optional<ButtonType> alertBuildForEdits(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("Leere Felder können nicht gespeichert werden!");
+
+        return alert.showAndWait();
+    }
+
+    /**
+     * This method handles events fired by the button to delete patients or Caregivers. It opens an alert to reassure the infinitely deletion of the patients data.
+     * On confirmation (press 'OK')  {@link } is called.
+     */
+
+    public static Optional<ButtonType> alertForDelete() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Daten endgültig löschen?");
+
+        return alert.showAndWait();
+    }
+}
