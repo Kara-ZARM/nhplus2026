05ab64562a51b539599ee09048f4f49428441f42
Marco Dittrich <marco.d2@schule.bremen.de>
2026-06-11 16:13:18 +0200

    added basic DBLogger to the utils

---
+++ src/main/java/de/hitec/nhplus/utils/DBLogger.java
@@ -0,0 +1,24 @@
+package de.hitec.nhplus.utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * The utility class for logging database-changes to a log file locally in the db/ folder.
+ */
+public class DBLogger {
+    private static final String LOG_FILE = "db/dbChanges.log";
+
+    /**
+     * A Filewriter is opened to write to local disk space. Messages are seperated by the systems line seperator.
+     * The log file is appending to the file if it is already existing.
+     * @param message The message that is written into the log file
+     */
+    public static void log(String message) {
+        try (FileWriter fileWriter = new FileWriter(LOG_FILE,true)) {
+            fileWriter.write(message + System.lineSeparator());
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
