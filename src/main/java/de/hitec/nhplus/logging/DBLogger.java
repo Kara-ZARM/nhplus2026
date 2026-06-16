06ba0a73fc49f5125451ad344fc27a9ba04bce43
Marco Dittrich <marco.d2@schule.bremen.de>
2026-06-16 08:48:29 +0200

    updated doc

--- src/main/java/de/hitec/nhplus/logging/DBLogger.java
+++ src/main/java/de/hitec/nhplus/logging/DBLogger.java
@@ -5,14 +5,17 @@ import java.io.IOException;
 
 /**
  * The utility class for logging database-changes to a log file locally.
+ * This class can be statically called to provide the log() method.
+ * Normally this is used after the creation, modification or deletion of database objects.
  */
 public class DBLogger {
     private static final String LOG_FILE = "db/dbChanges.log";
 
     /**
-     * A Filewriter is opened to write to local disk space. Messages are seperated by the systems line seperator.
+     * A Filewriter is opened to write to local disk space. Messages are created in form of <class>LogEntry</class> objects
+     * and seperated by the systems line seperator.
      * The log file is appending to the file if it is already existing.
-     * @param logEntry The LogEntry Object which information should be persisted in the log file.
+     * @param logEntry The <class>LogEntry</class> Object which information should be persisted in the log file.
      */
     public static void log(LogEntry logEntry) {
         System.out.println();
